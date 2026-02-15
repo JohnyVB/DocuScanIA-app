dc025c8024c12fb5ce285e9fdc30484d4f2d4277
Johny Villegas <johny.villegas.dev@gmail.com>
2026-02-15 18:17:30 +0100

    Settings with camera permission

--- app.config.ts
+++ app.config.ts
@@ -1,33 +1,32 @@
 import "dotenv/config";
 
 export default {
-    expo: {
-        name: "DocuScanAI-app",
-        slug: "DocuScanAI-app",
-        version: "1.0.0",
-        orientation: "portrait",
-        icon: "./assets/icon.png",
-        ios: { supportsTablet: true },
-        android: {
-            adaptiveIcon: {
-                foregroundImage: "./assets/adaptive-icon.png",
-                backgroundColor: "#FFFFFF",
-            },
-        },
-        extra: {
-            eas: { projectId: "b239b539-c609-4824-8322-c6f811c0830f" },
-            apiBaseUrl: process.env.API_BASE_URL,
-            apiLoginUrl: process.env.API_LOGIN_URL,
-            apiRegisterUrl: process.env.API_NEW_USER_URL,
-            apiSendResetPasswordCodeUrl:
-                process.env.API_SEND_RESET_PASSWORD_CODE_URL,
-            apiVerifyResetPasswordCodeUrl:
-                process.env.API_VERIFY_RESET_PASSWORD_CODE_URL,
-            apiResetPasswordUrl: process.env.API_RESET_PASSWORD_URL,
-            apiUploadDocumentUrl: process.env.API_UPLOAD_DOCUMENT_URL,
-            apiDocumentsByUserId: process.env.API_DOCUMENTS_BY_USER_ID,
-            apiDeleteDocumentById: process.env.API_DELETE_DOCUMENT_BY_ID,
-        },
-        plugins: ["expo-localization", "expo-font"],
+  expo: {
+    name: "DocuScanAI",
+    slug: "DocuScanAI",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    ios: { supportsTablet: true },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
     },
+    extra: {
+      eas: { projectId: "b239b539-c609-4824-8322-c6f811c0830f" },
+      apiBaseUrl: process.env.API_BASE_URL,
+      apiLoginUrl: process.env.API_LOGIN_URL,
+      apiRegisterUrl: process.env.API_NEW_USER_URL,
+      apiSendResetPasswordCodeUrl: process.env.API_SEND_RESET_PASSWORD_CODE_URL,
+      apiVerifyResetPasswordCodeUrl:
+        process.env.API_VERIFY_RESET_PASSWORD_CODE_URL,
+      apiResetPasswordUrl: process.env.API_RESET_PASSWORD_URL,
+      apiUploadDocumentUrl: process.env.API_UPLOAD_DOCUMENT_URL,
+      apiDocumentsByUserId: process.env.API_DOCUMENTS_BY_USER_ID,
+      apiDeleteDocumentById: process.env.API_DELETE_DOCUMENT_BY_ID,
+    },
+    plugins: ["expo-localization", "expo-font"],
+  },
 };
--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -3,14 +3,14 @@ import { Ionicons } from "@expo/vector-icons";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import React, { useEffect, useRef, useState } from "react";
 import {
-    ActivityIndicator,
-    FlatList,
-    Image,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DocumentDetailCard from "../components/ScanScreen/DocumentDetailCard";
@@ -22,153 +22,149 @@ import ScanStyles from "../styles/ScanStyles";
 import { DocumentTypes } from "../types/DocumentType";
 
 export const ScanScreen = () => {
-    const { token } = userStore();
-    const { colors } = useTheme();
-    const styles = ScanStyles(colors);
-    const [permission, requestPermission] = useCameraPermissions();
-    const [loading, setLoading] = useState<boolean>(false);
-    const cameraRef = useRef<CameraView>(null);
-    const [photos, setPhotos] = useState<string[]>([]);
-    const [document, setDocument] = useState<DocumentTypes | null>(null);
-    const { documents, setDocuments } = documentStore();
-    const [scanStatus, setScanStatus] = useState<boolean>(true);
-    const [errorMessage, setErrorMessage] = useState<string>("");
+  const { token } = userStore();
+  const { colors } = useTheme();
+  const styles = ScanStyles(colors);
+  const [permission, requestPermission] = useCameraPermissions();
+  const [loading, setLoading] = useState<boolean>(false);
+  const cameraRef = useRef<CameraView>(null);
+  const [photos, setPhotos] = useState<string[]>([]);
+  const [document, setDocument] = useState<DocumentTypes | null>(null);
+  const { documents, setDocuments } = documentStore();
+  const [scanStatus, setScanStatus] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-    useEffect(() => {
-        if (!permission) {
-            requestPermission();
-        }
-    }, []);
+  const sendPictures = async () => {
+    setScanStatus(false);
+    setLoading(true);
+    const data = await onUploadDocument(photos, token!);
+    if (data.status === "success") {
+      const newDocument: DocumentTypes = data.newDoc;
+      setDocument(newDocument);
+      setDocuments([newDocument, ...documents]);
+      setLoading(false);
+      setPhotos([]);
+    } else {
+      setLoading(false);
+      setErrorMessage(data.message);
+    }
+  };
 
-    const sendPictures = async () => {
-        setScanStatus(false);
-        setLoading(true);
-        const data = await onUploadDocument(photos, token!);
-        if (data.status === "success") {
-            const newDocument: DocumentTypes = data.newDoc;
-            setDocument(newDocument);
-            setDocuments([newDocument, ...documents]);
-            setLoading(false);
-            setPhotos([]);
-        } else {
-            setLoading(false);
-            setErrorMessage(data.message);
-        }
-    };
+  const takePicture = async () => {
+    if (!cameraRef.current) return;
+    const photo = await cameraRef.current.takePictureAsync({
+      quality: 0.8,
+      skipProcessing: false,
+    });
+    setPhotos((prev: string[]) => [...prev, photo.uri]);
+  };
 
-    const takePicture = async () => {
-        if (!cameraRef.current) return;
-        const photo = await cameraRef.current.takePictureAsync({
-            quality: 0.8,
-            skipProcessing: false,
-        });
-        setPhotos((prev: string[]) => [...prev, photo.uri]);
-    };
-
-    return (
-        <SafeAreaView style={styles.container} edges={["top"]}>
-            <Text style={styles.title}>{i18n.t("ScanScreen.title")}</Text>
-            {scanStatus && !loading && !document ? (
-                <View style={[styles.body, styles.shadow]}>
-                    <View style={StyleSheet.absoluteFill}>
-                        <CameraView
-                            ref={cameraRef}
-                            style={StyleSheet.absoluteFill}
-                            facing="back"
-                        />
-                        <View style={styles.overlay}>
-                            <View style={styles.scanBox} />
-                        </View>
-                        <TouchableOpacity
-                            style={styles.captureBtn}
-                            onPress={takePicture}>
-                            <Ionicons name="camera" size={24} color="#fff" />
-                        </TouchableOpacity>
-                        <View style={styles.previewContainer}>
-                            <FlatList
-                                data={photos}
-                                keyExtractor={(item, index) => item + index}
-                                horizontal
-                                showsHorizontalScrollIndicator={false}
-                                renderItem={({ item, index }) => (
-                                    <View style={styles.previewItem}>
-                                        <Image
-                                            source={{ uri: item }}
-                                            style={styles.previewImage}
-                                        />
-                                        {/* eliminar foto */}
-                                        <TouchableOpacity
-                                            style={styles.removePhotoBtn}
-                                            onPress={() =>
-                                                setPhotos((prev) =>
-                                                    prev.filter(
-                                                        (_, i) => i !== index,
-                                                    ),
-                                                )
-                                            }>
-                                            <Ionicons
-                                                name="close"
-                                                size={16}
-                                                color="#fff"
-                                            />
-                                        </TouchableOpacity>
-                                    </View>
-                                )}
-                            />
-                        </View>
-                        {photos.length > 0 && (
-                            <TouchableOpacity
-                                style={styles.sendBtn}
-                                onPress={sendPictures}>
-                                <Ionicons name="send" size={20} color="#fff" />
-                            </TouchableOpacity>
-                        )}
-                    </View>
-                </View>
-            ) : !scanStatus && loading ? (
-                <View style={[styles.body, styles.shadow]}>
-                    <View style={styles.activityContainer}>
-                        <ActivityIndicator size="large" color={colors.text} />
-                    </View>
-                </View>
-            ) : !scanStatus && !loading && document ? (
-                <ScrollView>
-                    <DocumentDetailCard document={document} />
+  return (
+    <SafeAreaView style={styles.container} edges={["top"]}>
+      <Text style={styles.title}>{i18n.t("ScanScreen.title")}</Text>
+      {!permission || !permission.granted ? (
+        <View style={[styles.body, styles.shadow]}>
+          <View style={[styles.activityContainer, { gap: 50 }]}>
+            <Text style={{ color: colors.text }}>
+              {i18n.t("ScanScreen.noPermissions")}
+            </Text>
+            <TouchableOpacity
+              style={styles.btnScan}
+              onPress={requestPermission}
+            >
+              <Text style={styles.textBtnScan}>
+                {i18n.t("ScanScreen.grantPermission")}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : scanStatus && !loading && !document ? (
+        <View style={[styles.body, styles.shadow]}>
+          <View style={StyleSheet.absoluteFill}>
+            <CameraView
+              ref={cameraRef}
+              style={StyleSheet.absoluteFill}
+              facing="back"
+            />
+            <View style={styles.overlay}>
+              <View style={styles.scanBox} />
+            </View>
+            <TouchableOpacity style={styles.captureBtn} onPress={takePicture}>
+              <Ionicons name="camera" size={24} color="#fff" />
+            </TouchableOpacity>
+            <View style={styles.previewContainer}>
+              <FlatList
+                data={photos}
+                keyExtractor={(item, index) => item + index}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item, index }) => (
+                  <View style={styles.previewItem}>
+                    <Image source={{ uri: item }} style={styles.previewImage} />
+                    {/* eliminar foto */}
                     <TouchableOpacity
-                        style={styles.btnScan}
-                        onPress={() => {
-                            setScanStatus(true);
-                            setDocument(null);
-                        }}>
-                        <Text style={styles.textBtnScan}>
-                            {i18n.t("ScanScreen.activateCamera")}
-                        </Text>
+                      style={styles.removePhotoBtn}
+                      onPress={() =>
+                        setPhotos((prev) => prev.filter((_, i) => i !== index))
+                      }
+                    >
+                      <Ionicons name="close" size={16} color="#fff" />
                     </TouchableOpacity>
-                </ScrollView>
-            ) : (
-                !scanStatus &&
-                !loading &&
-                !document && (
-                    <View style={[styles.body, styles.shadow]}>
-                        <View style={[styles.activityContainer, { gap: 50 }]}>
-                            <Text style={{ color: colors.text }}>
-                                {errorMessage}
-                            </Text>
-                            <TouchableOpacity
-                                style={styles.btnScan}
-                                onPress={() => {
-                                    setScanStatus(true);
-                                    setErrorMessage("");
-                                    setPhotos([]);
-                                }}>
-                                <Text style={styles.textBtnScan}>
-                                    {i18n.t("ScanScreen.activateCamera")}
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                )
+                  </View>
+                )}
+              />
+            </View>
+            {photos.length > 0 && (
+              <TouchableOpacity style={styles.sendBtn} onPress={sendPictures}>
+                <Ionicons name="send" size={20} color="#fff" />
+              </TouchableOpacity>
             )}
-        </SafeAreaView>
-    );
+          </View>
+        </View>
+      ) : !scanStatus && loading ? (
+        <View style={[styles.body, styles.shadow]}>
+          <View style={styles.activityContainer}>
+            <ActivityIndicator size="large" color={colors.text} />
+          </View>
+        </View>
+      ) : !scanStatus && !loading && document ? (
+        <ScrollView>
+          <DocumentDetailCard document={document} />
+          <TouchableOpacity
+            style={styles.btnScan}
+            onPress={() => {
+              setScanStatus(true);
+              setDocument(null);
+            }}
+          >
+            <Text style={styles.textBtnScan}>
+              {i18n.t("ScanScreen.activateCamera")}
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      ) : (
+        !scanStatus &&
+        !loading &&
+        !document && (
+          <View style={[styles.body, styles.shadow]}>
+            <View style={[styles.activityContainer, { gap: 50 }]}>
+              <Text style={{ color: colors.text }}>{errorMessage}</Text>
+              <TouchableOpacity
+                style={styles.btnScan}
+                onPress={() => {
+                  setScanStatus(true);
+                  setErrorMessage("");
+                  setPhotos([]);
+                }}
+              >
+                <Text style={styles.textBtnScan}>
+                  {i18n.t("ScanScreen.activateCamera")}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )
+      )}
+    </SafeAreaView>
+  );
 };
